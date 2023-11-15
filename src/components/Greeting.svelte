4a9479d1cd4b66fdf7e7dcf878eb19e250b559eb
Mike <miguel.suarezf05@gmail.com>
2023-11-14 22:24:11 -0500

    added react and created greeting component

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if (currentHour < 12) {
+    greeting = "Buenos Días"
+  } else if (currentHour < 18) {
+    greeting = "Buenos Tardes"
+    
+  } else {
+    greeting = "Buenos noches"  
+  }
+</script>
+
+<h3 class="text-3xl font-bold my-5">{greeting}</h3>
